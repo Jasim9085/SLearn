875cd6f0b671bd50548a442eb190e837c12591f9
Jasim9085 <juahmed9085@gmail.com>
2025-10-19 10:06:42 +0530

    update app/src/main/cpp/engine/include/t760_engine/platform/IPlatformMemory.h

--- app/src/main/cpp/engine/include/t760_engine/platform/IPlatformMemory.h
+++ app/src/main/cpp/engine/include/t760_engine/platform/IPlatformMemory.h
@@ -1,15 +1,17 @@
 #ifndef T760_IPLATFORM_MEMORY_H
-#define T760_IPLATFORM_MEMORY _H
+#define T760_IPLATFORM_MEMORY_H
 
 #include "t760_engine/memory/MemoryTypes.h"
-#include <memory> namespace t760 {
+#include <memory>
 
-// Interface for a device-specific memory allocator
+namespace t760 {
+
 class IPlatformMemory {
-public: virtual ~IPlatformMemory() = default;
-    virtual std::unique_ptr<Buffer> allocate(size _t size, MemoryUsage usage) = 0;
+public:
+    virtual ~IPlatformMemory() = default;
+    virtual std::unique_ptr<Buffer> allocate(size_t size, MemoryUsage usage) = 0;
 };
 
 }
 
-#endif // T760_ IPLATFORM_MEMORY_H 
+#endif // T760_IPLATFORM_MEMORY_H
